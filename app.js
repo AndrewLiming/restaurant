900796ffa1a4f11baef2f1d28b21869aae339970
Andrew Liming <andrewliming206@gmail.com>
2020-09-17 15:31:16 -0700

    added javascript

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-var userName = prompt('Please enter your name');
+function askUserNameQuestion(){
+  var userName = prompt('Please enter your name');
+var greeting = 'Hello ' + userName; 
+  document.write(greeting);
+      askUserAgePrompt();
+}
 
-document.write('Welcome' + userName);
+function askUserAgePrompt(){
+  var confirmAnswer = confirm('Press "ok" if over 21');
+    if (confirmAnswer){
+       alert('Please Visit Our Bar');
+   } else {
+       alert('Please Visit our family dining area')
+   }
+}
+
+// var today = new Date();
+var hourNow = prompt('What time is it, friend? 0-23'); 
+askUserNameQuestion();
+var greeting;
+
+if (hourNow > 18 && hourNow < 24) {
+  greeting = 'Good evening!  ';
+} else if (hourNow > 12 && hourNow < 24) {
+  greeting = 'Good afternoon!  ';
+} else if (hourNow >= 0 && hourNow < 24) {
+  greeting = 'Good morning!  ';
+} else {
+  greeting = 'Something went wrong!  ';
+}
+
+document.write(greeting);
+
